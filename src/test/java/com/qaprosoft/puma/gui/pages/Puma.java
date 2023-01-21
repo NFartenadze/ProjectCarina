4848a7e4f32e578c48bac654da01da11efbef3d6
NFartenadze <nfartenadze@solvd.com>
2023-01-21 11:46:50 +0400

    test

--- src/test/java/com/qaprosoft/puma/gui/pages/Puma.java
+++ src/test/java/com/qaprosoft/puma/gui/pages/Puma.java
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 
 public class Puma implements IAbstractTest {
-    //testing onpush
+    //testing
 
     @Test
     public void search() {
