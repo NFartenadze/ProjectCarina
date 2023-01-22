edcfefe1287314d551def2934c6240e4516a5108
NFartenadze <nfartenadze@solvd.com>
2023-01-22 13:25:33 +0400

    test

--- src/test/java/com/qaprosoft/puma/gui/pages/Puma.java
+++ src/test/java/com/qaprosoft/puma/gui/pages/Puma.java
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 
 public class Puma implements IAbstractTest {
-    //testing webhookasdasdasdasdasddasasdasdasddsasdsdsdssdsds
+    //testing webhookasdasdasdasdasddasasdasdasddsasdsdsdssdsdssdsdsdsdsdsdsdsd
 
     @Test
     public void search() {
