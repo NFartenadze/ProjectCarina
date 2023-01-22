7e0fd04373b27509b2a69aa170d1216c9db05460
NFartenadze <nfartenadze@solvd.com>
2023-01-22 16:10:36 +0400

    testing webhook

--- src/test/java/com/qaprosoft/puma/mobile/gui/pages/android/PumaTests.java
+++ src/test/java/com/qaprosoft/puma/mobile/gui/pages/android/PumaTests.java
@@ -12,7 +12,7 @@ import org.testng.annotations.Test;
 
 
 public class PumaTests implements IAbstractTest, IMobileUtils {
-///sdsdsasassdsds
+///sdsdsasassdsdssdsds
 
     @Test
     public void searchItem() {
